9e0dbe75e29763dc99e8f86cbd166fed26d458ac
neverjust <312726839@qq.com>
2018-12-19 20:03:39 +0800

    ldap

--- application/controller/User.php
+++ application/controller/User.php
@@ -31,7 +31,7 @@ class User extends Controller
     {
         $studentInfo = new Ldap($_POST['studentId'],$_POST['password']);
         $res = $studentInfo->run();
-        var_dump($res)；
+        var_dump($res);
     }
 
     public function login()
